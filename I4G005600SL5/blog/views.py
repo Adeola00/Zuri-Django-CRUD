24cbfbe7be9df7641d5b5da9c5264ecfc45fe4d6
Adeola Odusami <53563875+Adeola00@users.noreply.github.com>
2022-06-23 18:48:44 +0100

    Update views.py

--- I4G005600SL5/blog/views.py
+++ I4G005600SL5/blog/views.py
@@ -11,19 +11,24 @@ class PostCreateView(CreateView):
     model = Post
     fields = "__all__"
     success_url  = reverse_lazy("blog:all")
+    template_name = "post_form.html"
 
 class PostListView(ListView):
     model = Post
+    template_name = "post_list.html"
+
 
 class PostDetailView(DetailView):
     model = Post
-    
+    template_name = "post_detail.html"
 class PostUpdateView(UpdateView):
     model = Post
     fields = "__all__"
     success_url  = reverse_lazy("blog:all")
+    template_name = "post_form.html"
 
 class PostDeleteView(DeleteView):
     model = Post
     fields = "__all__"
     success_url  = reverse_lazy("blog:all")
+    template_name = "post_confirm_delete.html"
